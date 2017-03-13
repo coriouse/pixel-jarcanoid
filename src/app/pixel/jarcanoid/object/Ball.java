ac531a774a57f2146f215403cd8aa8953ae9845d
Ogarkov Sergey <coriouse@gmail.com>
2017-03-13 13:03:56 +0300

    walls

--- src/app/pixel/jarcanoid/object/Ball.java
+++ src/app/pixel/jarcanoid/object/Ball.java
@@ -25,39 +25,63 @@ public class Ball extends Mob {
 
 	public void update(float deltaTime) {
 
-		
-
 		// TODO need to resolve direction of the ball
-		System.out.println("direction == " + direction);
+		Sprite[] sprites = getColliders(posX, posY);
+		System.out.println("Platform.direction=[" + Platform.direction + "], direction=[" + direction + "],"
+				+ Arrays.toString(sprites)+", posY="+posY);
 		// up
-		if (doesCollide(posX, posY) && posY < 0) {
-			if (direction == Direction.UP_RIGHT) {
-				if ((Platform.direction == Direction.RIGHT) &&  getSprite(getColliders(posX, posY)) instanceof Proof) {
-					direction = Direction.DOWN_RIGHT;
-				} else {
-					direction = Direction.UP_LEFT;
-				}
-			} else if (direction == Direction.UP_LEFT) {
+		if (sprites.length > 0) {
 
-				if ((Platform.direction == Direction.LEFT) && getSprite(getColliders(posX, posY)) instanceof Proof) {
-					direction = Direction.DOWN_LEFT;
-				} else {
-					direction = Direction.UP_RIGHT;
+			if (posY < 0) {
+				if (direction == Direction.UP_RIGHT) {
+
+					if ((Platform.direction == Direction.RIGHT)/*
+																 * && getSprite(
+																 * sprites)
+																 * instanceof
+																 * Proof
+																 */) {
+						direction = Direction.DOWN_RIGHT;
+					} else {
+						direction = Direction.UP_LEFT;
+					}
+				} else if (direction == Direction.UP_LEFT) {
+					if ((Platform.direction == Direction.LEFT) /*
+																 * && getSprite(
+																 * sprites)
+																 * instanceof
+																 * Proof
+																 */) {
+						direction = Direction.DOWN_LEFT;
+					} else {
+						direction = Direction.UP_RIGHT;
+					}
 				}
-			}
-			// down
-		} else if (doesCollide(posX, posY) && posY > 0) {
-			if (direction == Direction.DOWN_LEFT) {
-				if (Platform.direction == Direction.LEFT && getSprite(getColliders(posX, posY)) instanceof Platform) {
-					direction = Direction.UP_LEFT;
-				} else {
-					direction = Direction.DOWN_RIGHT;
-				}
-			} else if (direction == Direction.DOWN_RIGHT) {
-				if (Platform.direction == Direction.RIGHT && getSprite(getColliders(posX, posY)) instanceof Platform) {
-					direction = Direction.UP_RIGHT;
-				} else {
-					direction = Direction.DOWN_LEFT;
+			} else if (posY > 0) {
+				// down
+				 
+				if (direction == Direction.DOWN_LEFT) {
+					if (Platform.direction == Direction.LEFT/*
+															 * && getSprite(
+															 * sprites)
+															 * instanceof
+															 * Platform
+															 */) {
+						direction = Direction.UP_LEFT;
+					} else {
+						direction = Direction.DOWN_RIGHT;
+					}
+				} else if (direction == Direction.DOWN_RIGHT) {
+					if (Platform.direction == Direction.RIGHT /*
+																 * && getSprite(
+																 * sprites)
+																 * instanceof
+																 * Platform
+																 */) {
+						direction = Direction.UP_RIGHT;
+					} else {
+						direction = Direction.DOWN_LEFT;
+					}
 				}
 			}
 		}
