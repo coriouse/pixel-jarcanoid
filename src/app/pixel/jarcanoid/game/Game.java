b546117f02619870310d8c82570e27c6ec033450
Ogarkov Sergey <coriouse@gmail.com>
2017-03-28 09:04:22 +0300

    refactoring

--- src/app/pixel/jarcanoid/game/Game.java
+++ src/app/pixel/jarcanoid/game/Game.java
@@ -28,62 +28,59 @@ public class Game {
 	public static void main(String[] args) {
 		Render.init();
 		Arena.currentArena = new Arena();
-		//init game
+		// init game
 		startGame();
 
 	}
-	
+
+	// TODO need to improve game cycle
 	public static void gameOver() {
 		Arena.currentArena.cleanSprites();
-		Arena.currentArena.addSprite(new GameOver(-130,0));
+		Arena.currentArena.addSprite(new GameOver(-130, 0));
 	}
-	
+
 	public static void reload() {
 		Arena.currentArena.cleanSprites();
 		startGame();
 	}
-	
+
 	public static void startGame() {
-				// Platform
-				Arena.currentArena.addSprite(new Platform(0, 200, 100, 20));
+		// Platform
+		Arena.currentArena.addSprite(new Platform(0, 200, 100, 20));
 
-				// Walls
-				Arena.currentArena.addSprite(new WallRight(390, 5, WIDTH_WALLS, Render.gameHeight - 35));
-				Arena.currentArena.addSprite(new WallLeft(-390, 5, WIDTH_WALLS, Render.gameHeight - 35));
-				// Proof
-				Arena.currentArena.addSprite(new Proof(0, -202, Render.gameWidth - 20, WIDTH_WALLS));
-				// Ground
-				Arena.currentArena.addSprite(new Ground(0, 230, Render.gameWidth + 200, 1));
+		// Walls
+		Arena.currentArena.addSprite(new WallRight(390, 5, WIDTH_WALLS, Render.gameHeight - 35));
+		Arena.currentArena.addSprite(new WallLeft(-390, 5, WIDTH_WALLS, Render.gameHeight - 35));
+		// Proof
+		Arena.currentArena.addSprite(new Proof(0, -202, Render.gameWidth - 20, WIDTH_WALLS));
+		// Ground
+		Arena.currentArena.addSprite(new Ground(0, 230, Render.gameWidth + 200, 1));
 
-				// Ball
-				Arena.currentArena.addSprite(new Ball(45, 200, 100, 40));
+		// Ball
+		Arena.currentArena.addSprite(new Ball(45, 200, 100, 40));
 
-				// blocks
-				//LEVEL 1
-				
-				// 1 row
-				Arena.currentArena.addSprite(new Block(0, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 0));
-				Arena.currentArena.addSprite(new Block(90, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 1));
-				Arena.currentArena.addSprite(new Block(-90, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 2));
-				Arena.currentArena.addSprite(new Block(-180, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 3));
-				Arena.currentArena.addSprite(new Block(180, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 4));
-				// 2 row
-				Arena.currentArena.addSprite(new Block(0, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 5));
-				Arena.currentArena.addSprite(new Block(90, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 6));
-				Arena.currentArena.addSprite(new Block(-90, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 7));
-				Arena.currentArena.addSprite(new Block(-180, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 8));
-				Arena.currentArena.addSprite(new Block(180, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 9));
-				
-				
-				Arena.currentArena.addSprite(new Lifes(-330, 220, 90, 20));
-				Arena.currentArena.addSprite(new Lifes(-310, 220, 90, 20));
-				Arena.currentArena.addSprite(new Lifes(-290, 220, 90, 20));
-				
-				Arena.currentArena.addSprite(new Score(-270, 225));	
+		// blocks
+		// LEVEL 1
+		// TODO create level need to refactor
+		// 1 row
+		Arena.currentArena.addSprite(new Block(0, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 0));
+		Arena.currentArena.addSprite(new Block(90, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 1));
+		Arena.currentArena.addSprite(new Block(-90, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 2));
+		Arena.currentArena.addSprite(new Block(-180, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 3));
+		Arena.currentArena.addSprite(new Block(180, -150, WIDTH_BLOCK, HEIGHT_BLOCK, 4));
+		// 2 row
+		Arena.currentArena.addSprite(new Block(0, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 5));
+		Arena.currentArena.addSprite(new Block(90, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 6));
+		Arena.currentArena.addSprite(new Block(-90, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 7));
+		Arena.currentArena.addSprite(new Block(-180, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 8));
+		Arena.currentArena.addSprite(new Block(180, -120, WIDTH_BLOCK, HEIGHT_BLOCK, 9));
+
+		Arena.currentArena.addSprite(new Lifes(-330, 220, 90, 20));
+		Arena.currentArena.addSprite(new Lifes(-310, 220, 90, 20));
+		Arena.currentArena.addSprite(new Lifes(-290, 220, 90, 20));
+
+		Arena.currentArena.addSprite(new Score(-270, 225));
 	}
-	
-	
-	
 
 	public static void quit() {
 		System.exit(1);
